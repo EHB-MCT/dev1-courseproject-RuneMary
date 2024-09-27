7efe1a060bf66ba93dfc714d5ab927764d5973b3
RuneM <rune.mary@student.ehb.be>
2024-09-27 14:50:30 +0200

    SpaceInvader Added

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,32 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+
+
+function background () {
+    context.fillStyle = "#f0f0f0";
+    context.beginPath();
+    context.fillRect(50,50,300,300)
+    context.stroke();
+}
+
+function rects () {
+    context.fillStyle = "#de9eae";
+    context.beginPath();
+    context.fillRect(75,75,50,50)
+    context.fillRect(125,125,50,50)
+    context.fillRect(225,125,50,50)
+    context.fillRect(275,75,50,50)
+    context.fillRect(75,175,250,50)
+    context.fillRect(75,225,50,50)
+    context.fillRect(275,225,50,50)
+    context.stroke();
+
+    
+}
+
+
+background();
+rects();
