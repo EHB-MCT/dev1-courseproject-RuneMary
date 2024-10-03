b42d7b67eb236b9f72159e723498e8259a0a41e1
RuneM <rune.mary@student.ehb.be>
2024-10-03 13:10:03 +0200

    black background added

--- spaceInvader.js
+++ spaceInvader.js
@@ -3,30 +3,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext("2d");
 
-
-
-function background () {
-    context.fillStyle = "#f0f0f0";
-    context.beginPath();
-    context.fillRect(50,50,300,300)
-    context.stroke();
+function background() {
+	context.beginPath();
+	context.fillRect(50, 50, 300, 300);
+	context.stroke();
 }
 
-function rects () {
-    context.fillStyle = "#de9eae";
-    context.beginPath();
-    context.fillRect(75,75,50,50)
-    context.fillRect(125,125,50,50)
-    context.fillRect(225,125,50,50)
-    context.fillRect(275,75,50,50)
-    context.fillRect(75,175,250,50)
-    context.fillRect(75,225,50,50)
-    context.fillRect(275,225,50,50)
-    context.stroke();
-
-    
+function rects() {
+	context.fillStyle = "#de9eae";
+	context.beginPath();
+	context.fillRect(75, 75, 50, 50);
+	context.fillRect(125, 125, 50, 50);
+	context.fillRect(225, 125, 50, 50);
+	context.fillRect(275, 75, 50, 50);
+	context.fillRect(75, 175, 250, 50);
+	context.fillRect(75, 225, 50, 50);
+	context.fillRect(275, 225, 50, 50);
+	context.stroke();
 }
-
 
 background();
 rects();
