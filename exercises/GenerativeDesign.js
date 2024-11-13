722bbeaff5d665fccf1081fc646b5a77743d3f7b
RuneM <rune.mary@student.ehb.be>
2024-11-13 11:43:46 +0100

    GenerativeDesign change2

--- exercises/GenerativeDesign.js
+++ exercises/GenerativeDesign.js
@@ -4,3 +4,22 @@ import * as Utils from "../scripts/utils.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
+
+rect();
+
+function rect() {
+	context.lineWidth = 2;
+	context.fillRect(0, 0, width, height);
+	DrawLines();
+}
+
+function DrawLines() {
+	for (let i = 0; i < 100; i++) {
+		let x = Math.random() * width;
+		let y = Math.random() * height;
+		context.strokeStyle = "white";
+		Utils.drawLine(x, y, width / 2, height / 2);
+		i++;
+		console.log(i);
+	}
+}
