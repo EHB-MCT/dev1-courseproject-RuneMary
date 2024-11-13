aabf77989da3f6098634dd01c7c11f45c561add2
RuneM <rune.mary@student.ehb.be>
2024-11-13 11:56:34 +0100

    GenerativeDesign change6

--- exercises/GenerativeDesign.js
+++ exercises/GenerativeDesign.js
@@ -14,24 +14,16 @@ function rect() {
 }
 
 function DrawLines() {
-	for (let i = 0; i < 1000; i++) {
+	for (let i = 0; i < 40; i++) {
 		let x = Math.random() * width;
 		let y = Math.random() * height;
 
-		//let distance = Utils.calculateDistance(width / 2, height / 2, x, y);
-
-		//if (distance < 500) {
-		//	context.strokeStyle = Utils.hsl(Utils.randomNumber(0, 360), 50, 50);
-		//} else {
-		//	context.strokeStyle = "white";
-		//}
-
 		let distance = Utils.calculateDistance(width / 2, height / 2, x, y);
 
-		if (distance < 300) {
-			context.strokeStyle = "white";
+		if (distance > 300) {
+			context.strokeStyle = Utils.hsl(Utils.randomNumber(0, 180), 50, 50);
 		} else {
-			context.strokeStyle = "red";
+			context.strokeStyle = Utils.hsl(Utils.randomNumber(180, 360), 50, 50);
 		}
 
 		Utils.drawLine(x, y, width / 2, height / 2);
